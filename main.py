a0d9e60d027e64e49f5dc453a7e36fe6e52d1cce
LEBaldy <ben.mathis02@gmail.com>
2021-10-18 23:13:21 -0600

    Update main.py

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ try:
   def zcalc(current, hoursleft, day):
     try:
       if (minutesremaining == 0):
-        stdvpredicted = stdv[hoursremaining - 1]
+        stdvpredicted = stdv[int(hoursremaining - 25)]
       else:
         stdvpredicted = ((stdv[math.ceil(hoursleft) - 1] -stdv[math.floor(hoursleft) - 1]) /(math.ceil(hoursleft) - math.floor(hoursleft))) * (hoursleft - math.floor(hoursleft)) + stdv[math.floor(hoursleft) - 1]
     except Exception as stdvrange:
@@ -78,7 +78,7 @@ try:
       stdvpredicted = 1.05977357360136 * 10 + 6.95546690304286 * 10**(-1) * hoursleft - 1.67571179241219 * 10**(-2) * hoursleft**2 + 5.05383084475387 * 10**(-4) * hoursleft**3 - 1.17803724420283 * 10**(-5) * hoursleft**4 + 1.9390541003977 * 10**(-7) * hoursleft**5 - 2.18168285848933 * 10**(-9) * hoursleft**6 + 1.62938690786485 * 10**(-11) * hoursleft**7 - 7.68703295853448 * 10**(-14) * hoursleft**8 + 2.06664417836592 * 10**(-16) * hoursleft**9 - 2.40708085158142 * 10**(-19) * hoursleft**10
     try:
       if (minutesremaining == 0):
-        stdvdone = stdv[(192 - hoursremaining) - 1]
+        stdvdone = stdv[int((192 - hoursremaining) - 25)]
       else:
         stdvdone = ((stdv[math.ceil(168 - hoursleft) - 1] -stdv[math.floor(168 - hoursleft) - 1]) /(math.ceil(168 - hoursleft) - math.floor(168 - hoursleft))) * (hoursleft - math.floor(hoursleft)) + stdv[math.floor(168 - hoursleft) - 1]
     except Exception as stdvrange:
